4d737cdb4701f11320a421cd6a4c419c07197d5f
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-09-29 07:03:23 -0700

    tasks 06

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - description
+ *
+ * return : 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu bytes\n", sizeof(char));
+	printf("Size of an int: %zu bytes\n", sizeof(int));
+	printf("Size of a long int: %zu bytes\n", sizeof(long int));
+	printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
+	printf("Size of a float: %zu bytes\n", sizeof(float));
+return: (0);
+}
