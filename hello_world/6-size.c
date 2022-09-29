4eb1b4e1077ea60c1da61ae32e22b67b63b29953
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-09-29 07:29:55 -0700

    tasks 06

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - description
+ * main - print bytes variables
  *
- * Return:0
+ * Return: Always 0 (Success)
  */
 
-int main(void)
+        char
+	int
+	long int
+	long long int
+       	float
+
+|int main(void)
 {
 	printf("Size of a char: %zu bytes\n", sizeof(char));
 	printf("Size of an int: %zu bytes\n", sizeof(int));
 	printf("Size of a long int: %zu bytes\n", sizeof(long int));
 	printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
 	printf("Size of a float: %zu bytes\n", sizeof(float));
-return:(0);
+return (0);
 }
