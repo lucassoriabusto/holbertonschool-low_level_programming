009eccf7d0960f2ee960959e560f517a835acd8c
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-09-30 11:59:27 -0700

    Tasks 03

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - description
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a' ; n <= 'z' ; ++n)
+	{
+		putchar(n);
+	}
+	for (n = 'A' ; n <= 'Z' ; ++n)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
