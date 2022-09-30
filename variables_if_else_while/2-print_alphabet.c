5e774b3ae5e3fc3ba98da78ef722dda15f0ffe7c
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-09-30 11:25:35 -0700

    Tastk 02

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * main - description
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a' ; n <= 'z' ; ++n)
+	putchar (n);
+	putchar ('\n');
+	return (0);
+}
