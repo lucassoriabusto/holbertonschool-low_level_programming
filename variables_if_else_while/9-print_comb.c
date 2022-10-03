22ccd6f285cfef36d1ab4a0b40f0149cb5f943f4
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-02 17:30:48 -0700

    Tasks 09

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar (num % 10 + '0');
-		if (num == 10)
+		if (num == 9)
+			continue;
 
 		putchar(',');
 		putchar(' ');
 	}
-
 	putchar ('\n');
 	return (0);
 }
