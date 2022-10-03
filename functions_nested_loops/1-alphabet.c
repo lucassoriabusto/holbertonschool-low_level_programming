854a52d08fda2a617ccb6deab5e80d7266e76f54
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-03 12:44:25 -0700

    Tasks 01

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#iinclude "main.h"
+/**
+ *  main - description
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char n;
+	for (n = 'a' ; n <= 'z' ; ++n)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
