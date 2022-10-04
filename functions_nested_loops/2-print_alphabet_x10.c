e95fcad44f10698a408d904641c5bd16b31d3e55
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-03 19:53:42 -0700

    Tasks 02

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - description
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char n;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	for (n = 'a' ; n <= 'z' ; ++n)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
