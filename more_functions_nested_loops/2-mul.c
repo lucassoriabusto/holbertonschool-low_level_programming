552f5ca1a8ba00acffa3391659b26d018750d7fa
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-05 12:42:53 -0700

    Tasks 02

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: number
+ * @b: number
+ * Return: result of the multiplication
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
