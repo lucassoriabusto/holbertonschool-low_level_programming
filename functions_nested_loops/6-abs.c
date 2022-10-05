c30c391267fee34e8e69233ba15a9b0f83966bfa
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-04 18:02:35 -0700

    Tasks 06

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: is a integer number resived
+ * Return: less than 0 absolute value,greater than 0 the number, otherwise 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+
+}
