f711da8610adab3e894aa11fa8b257d466e0607a
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-05 06:36:57 -0700

    Tasks 07

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: number received
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -21,4 +21,7 @@ int print_sign(int n);
 
 int _abs(int);
 
+int print_last_digit(int);
+
 #endif
+
