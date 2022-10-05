902ee847523c0e48ed78fdb50bf5229092a9f078
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-05 12:59:35 -0700

    Tasks 03

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		_putchar("%i", i);
+	_putchar ('\n');
+	return (0);
+}
