9c208675cc8137a19208d0b602ed9793ce54df46
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-05 18:12:35 -0700

    Tasks 08

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: is the size of the square
+ */
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y < size; y++)
+			{
+				_putchar('*');
+			}
+			_putchar('*');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
