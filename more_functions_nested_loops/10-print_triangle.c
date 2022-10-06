a5cc05d8ba34224ee2dee3d2a3f9f5d769b0b1c0
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-05 18:52:53 -0700

    Tasks 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * print_triangle - Prints a triangle of squares according parameter
+  * @size: The size of the squares triangle
+  *
+  * Return: empty
+  */
+void print_triangle(int size)
+{
+	int x;
+	int y;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= size; y++)
+			{
+				if (y != size && y <= size - x)
+				{
+					_putchar(' ');
+				}
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		 _putchar('\n');
+	}
+}
