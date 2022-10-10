20f74e35246b05f9a8f2086cbb8205c9874d23fc
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-10 13:53:41 -0700

    Tasks 00

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
+ * reset_to_98 - Updates the value it points to 98
+ * @n: pointer to n
  *
- * Description
- *
- *
+ * Return: void
  */
 
 void reset_to_98(int *n)
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -27,3 +27,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
+
+#endif
