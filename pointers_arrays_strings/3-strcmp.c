921593446e1acba683ef5ae12123e197905c2ac9
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-13 16:32:45 -0700

    Tasks 07

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,31 +1,17 @@
 #include "main.h"
-
 /**
-  * *_strspn - gets the length of a prefix substring.
-  * @s: String parameter.
-  * @accept: Substring parameter.
-  *
-  * Description: gets the length of a prefix substring.
-  * Return: The number of bytes in the initial segment of s which consist onlyi
-  * of bytes from accept.
-  */
-
-unsigned int _strspn(char *s, char *accept)
+ * _strcmp - compares two strings
+ * @s1: string parameter
+ * @s2: string parameter to compares
+ *
+ *Return: 0
+ */
+int _strcmp(char *s1, char *s2)
 {
-	unsigned int count = 0, i = 0, j = 0;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] != 32)
-		{
-			for (j = 0; accept[j] != '\0'; j++)
-			{
-				if (s[i] == accept[j])
-					count++;
-			}
-		}
-		else
-			return (count);
-	}
-	return (count);
+	for (; s1[i] && s2[i]; i++)
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	return (0);
 }
