8420797ce433c078d7d348fae2c30465a494b8bb
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-16 18:29:14 -0700

    Tasks 00

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed a new line
+ * @s: ponter to the string
+ */
+void _puts_recursion(char *s)
+{
+	if
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('/n');
+}
