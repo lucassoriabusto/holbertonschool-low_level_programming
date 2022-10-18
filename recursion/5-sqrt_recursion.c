99b0041484a0f4bb8b2c0ad89aa1e1237a1f4c9f
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-17 19:19:23 -0700

    Tasks 00

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+int _sqrt_recursion(int n)
+{
+	if (n * n == 0)
+		return (-1);
+
+	if (n * n < 0)
+		return (0);
+
+
+int _sqrt_recursion(int n)
+{
+	_sqrt_recursion (
