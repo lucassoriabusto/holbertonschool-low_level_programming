d1ebbd7482c9dff85a767c359b73c9f50d0ef485
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-18 18:43:38 -0700

    Tasks 00

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * main - prints its name, followed by a new line.
+  * @argc: Count number of arguments.
+  * @argv: Array of strings (arguments).
+  *
+  * Return: 0.
+  */
+
+int main(int argc, char **argv)
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
