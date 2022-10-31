37048d5fc89b8ef7827d5e3229e9f7703151c186
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-30 18:08:09 -0700

    Tasks 02

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,8 +16,10 @@ void print_dog(struct dog *d)
 	else
 	{
 		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
 	}
+	if (d->age)
+		printf("Age: %f\n", d->age);
+
 	if (d->owner == NULL)
 	{
 		printf("(nil)");
