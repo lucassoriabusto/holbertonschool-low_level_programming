51ea3b89297c3e32695d08f49f53bf3d1a1bda21
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-10-30 20:11:10 -0700

    Tasks 03

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -12,9 +12,8 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+} my_dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-void free_dog(dog_t *d);
 #endif
