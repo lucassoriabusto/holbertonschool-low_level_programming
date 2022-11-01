7d37b8370a2ebff5f2f718f14b66df925c9a4c50
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-11-01 16:23:58 -0700

    Tasks 00

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,15 @@
-ime(char *name, void (*f)(char *))
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - Print a name
+ * @name:name
+ * @f:ponter to function
+ *
+ */
+void print_name(char *name, void (*f)(char *))
 {
-
+	if (f != NULL)
+	{
+		(f)(name);
+	}
+}
