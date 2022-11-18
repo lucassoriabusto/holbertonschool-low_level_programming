565dd2a9b563c29efa7285d45b1533da2e0c63aa
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-11-18 11:49:56 -0800

    Tasks 01

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 /**
- *
+ * list_len - returns the number of elements in a linked 
+ * @h: is the pointer to a structure of a linked list
+ * Return: the number of nodes
  *
  */
 size_t list_len(const list_t *h)
