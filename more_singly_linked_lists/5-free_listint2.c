fed39aae9f312ef369e33052173ace8c0c2f3f1c
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-11-20 19:00:22 -0800

    Tasks 05

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,13 +1,16 @@
 #include "lists.h"
+#include "4-free_listint.c"
+
 /**
-* free_listint2 - frees a list and sets head to NULL
-* @head: pointer to thr first node of a list
+*free_listint2 - free list
+*
+*@head: pointer to firts noede
 */
+
 void free_listint2(listint_t **head)
 {
 	if (head)
 	{
-		free(head->next);
 		free_listint(*head);
 		*head = NULL;
 	}
