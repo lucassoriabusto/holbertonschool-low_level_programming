9dbb5ce93dd0e8172bdd16136b2ea3f2247ddbbd
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-11-20 18:45:47 -0800

    Tasks 00

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* print_listint - print all elements of a listin_t list
+* @h: head to pointer
+*
+* Return: the number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	for (i = 0; h != NULL; h = h->next)
+	{
+		printf("%d\n", h->n);
+			i++;
+
+	}
+	return (i);
+}
