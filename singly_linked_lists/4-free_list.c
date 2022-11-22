5b2647958a56c9e854a058943a4e8b88994237cc
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-11-22 05:35:48 -0800

    Tasks 04wq

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
 *free_list - free list
 * @head: pointer to the list
@@ -8,8 +7,7 @@ void free_list(list_t *head)
 {
 	if (head)
 	{
-		free_list(head->next);
+		free(head->next);
 		free(head->str);
-		free(head);
 	}
 }
