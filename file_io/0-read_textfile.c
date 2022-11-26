525072672c0dd96d076b8e50deba6521cc4491d8
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-11-25 17:47:37 -0800

    Tasks 00

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+* read_textfile - reads a text file and prints it to the POSIX standard output
+* @filename: file name
+* @letters:number of letters it should read and prin
+* Return: the actual number of letters it could read and print
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+
+	int fd;
+	char *buf;
+	int r;
+	int w;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(letters);
+	if (buf == NULL)
+		return (0);
+	r = read(fd, buf, letters);
+	if (r == -1)
+		return (0);
+
+	w = write(1, buf, r);
+	if (w == -1)
+		return (0);
+	close(fd);
+	free(buf);
+	return (r);
+}
---
+++ file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
