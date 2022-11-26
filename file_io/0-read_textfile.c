a3eebff3d5b245ff62ccfa896c47ad34559a0b2e
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-11-25 18:07:49 -0800

    Tasks 00

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -22,14 +22,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buf = malloc(letters);
 	if (buf == NULL)
 		return (0);
+
 	r = read(fd, buf, letters);
 	if (r == -1)
 		return (0);
+	close(fd);
 
 	w = write(1, buf, r);
 	if (w == -1)
 		return (0);
-	close(fd);
+
 	free(buf);
 	return (r);
 }
