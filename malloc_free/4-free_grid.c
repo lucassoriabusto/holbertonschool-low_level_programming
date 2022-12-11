eb878b740ab14512ed285b48c7a4c9edb026017a
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-12-11 11:21:51 -0800

    Task

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,16 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * free_grid - frees a 2D grid previously created by alloc_grid function.
- * @grid: 2D grid to free
- * @height: number of columns of the grid
- *
- */
+  * free_grid - frees a 2D grid previously created by alloc_grid function.
+  * @grid: 2D grid to free.
+  * @height: Number of columns of the grid.
+  *
+  */
+
 void free_grid(int **grid, int height)
 {
 	int i;
 
-	for (i = 0; i <= height; i++)
+	for (i = 0; i < height; i++)
 		free(grid[i]);
 	free(grid);
 }
